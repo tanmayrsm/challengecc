53fa11198bd74632dad48d307ea022bbcbe5f380
tanmayrsm <45991527+tanmayrsm@users.noreply.github.com>
2019-09-10 20:20:16 +0530

    Add files via upload

---
+++ Scream.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import withStyles from '@material-ui/core/styles/withStyles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Typography from '@material-ui/core/Typography';
+import {Link} from 'react-router-dom';
+import dayjs from 'dayjs';
+
+const styles = {
+    card : {
+        display : 'flex',
+        marginBottom : 20
+    },
+    image : {
+        minWidth : 200
+    },
+    content :{
+        padding : 25,
+        objectFit : 'cover'
+    }
+}
+class Scream extends Component {
+    render() {
+        const { classes ,scream : { body ,createdAt ,userImage ,userHandle ,screamId ,likeCount ,commentCount } } = this.props;
+        return (
+            <Card className = {classes.card}>
+                <CardMedia image = { userImage } title = "Profile Image" className = {classes.image}/>
+                    <CardContent className = {classes.content}>
+                        <Typography variant = "h5" component = {Link} to = {`/users/${userHandle}`} color = "primary">{ userHandle }</Typography>
+                        <Typography variant = "body2" color = "textSecondary">{ createdAt }</Typography>
+                        <Typography variant = "body1">{ body }</Typography>
+                    </CardContent>
+            </Card>
+        )
+    }
+}
+
+export default withStyles(styles)(Scream);
